24dd0285a28445b7e8c09f2c853f0fda94aedd71
jawc <joaoalfredowc@gmail.com>
2025-01-22 15:39:00 -0300

    finished

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function validaNum() {
-    
-    return nomeComoArray.length >= 2;
-}
+const form = document.getElementById('form-validacao');
+const numA = document.getElementById('numA');
+const numB = document.getElementById('numB');
+const mensagem = document.getElementById('mensagem');
 
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-})
+
+    const valorA = parseFloat(numA.value);
+    const valorB = parseFloat(numB.value);
+
+    if (valorB > valorA) {
+        mensagem.innerHTML = `Sucesso: O número <b>B: ${valorB}</b> é maior que <b>A: ${valorA}</b>.`;
+        mensagem.className = "message success";
+        mensagem.style.display = "block";
+    } else {
+        mensagem.innerHTML = `Erro: O número <b>B: ${valorB}</b> não é maior que <b>A: ${valorA}</b>.`;
+        mensagem.className = "message error";
+        mensagem.style.display = "block";
+    }
+});
